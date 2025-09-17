048b0db13eabe4c71fdf68d3c209e707985ca09b
Swantan Barua <work.swantan@gmail.com>
2025-09-17 19:55:02 +0530

    Configure Explore table view protocols

---
+++ AirBnb-UIKit/ExploreVC+TableView.swift
@@ -0,0 +1,37 @@
+//
+//  ExploreVC+TableView.swift
+//  AirBnb-UIKit
+//
+//  Created by Swantan Barua on 17/09/25.
+//
+
+import UIKit
+
+extension ExploreVC: UITableViewDelegate {
+    
+}
+
+extension ExploreVC: UITableViewDataSource {
+    
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        return 10
+    }
+    
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: "ExploreTableViewCell",
+            for: indexPath
+        ) as? ExploreTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        return cell
+    }
+}
