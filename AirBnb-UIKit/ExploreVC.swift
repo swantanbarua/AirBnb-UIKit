581ea979a86e2e3c8b2ab8b0f99918ba043e9151
Swantan Barua <work.swantan@gmail.com>
2025-09-17 18:42:23 +0530

    Create ExploreVC

--- AirBnb-UIKit/ExploreVC.swift
+++ AirBnb-UIKit/ExploreVC.swift
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ExploreVC: UIViewController {
 
+    // MARK: - IBOUTLETS
+    
+    // MARK: - LIFECYCLE METHODS
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    // MARK: - IBACTIONS
 }
-
