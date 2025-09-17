0e28a2d2d44e64a4142f2556239e69367b9ec4d4
Swantan Barua <work.swantan@gmail.com>
2025-09-17 19:46:26 +0530

    ExploreTableViewCell added

---
+++ AirBnb-UIKit/ExploreTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ExploreTableViewCell.swift
+//  AirBnb-UIKit
+//
+//  Created by Swantan Barua on 17/09/25.
+//
+
+import UIKit
+
+class ExploreTableViewCell: UITableViewCell {
+
+    // MARK: - IBOUTLETS
+    
+    // MARK: - LIFECYCLE METHODS
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    // MARK: - IBACTIONS
+}
